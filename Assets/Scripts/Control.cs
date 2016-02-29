e48b0e732144226652afaa21b8138553f25bc687
eddcecc <cristhian.edic@gmail.com>
2016-02-28 21:28:12 -0500

    commit

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control : MonoBehaviour {
+    
+    public float speedX = 1.1f;
+    public int mov = 0;
+    private WaitForSeconds delay_time = new WaitForSeconds(0.00001f);
+    public float velocidad = 5.4f;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+            anim.SetFloat("speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+            float tecladoHorizontal = Input.GetAxisRaw("Horizontal");
+            float posX = (transform.position.x + (tecladoHorizontal * velocidad * Time.deltaTime));
+            transform.position = new Vector2(posX, GetComponent<Rigidbody2D>().position.y);
+    }
+
+
+
+
+
+}
---
+++ Assets/Walk.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : MonoBehaviour {
+
+    public float speed = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Comienzo aqui");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        float posX = (transform.position.x + (horizontal * speed * Time.deltaTime));
+        transform.position = new Vector2(posX, 0);
+        
+    }
+}
